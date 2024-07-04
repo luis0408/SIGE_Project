6b65b3247e51f28efcc31fae6fe9b38c1e495ec1
Alejandro Pérez Silva <alejo_lui@hotmail.com>
2024-07-03 22:20:55 -0600

    Form de datos de estado civil

---
+++ SIGE_Project/DatosEstadoCivil.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace SIGE_Project
+{
+    partial class DatosEstadoCivil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DatosEstadoCivil));
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.textEdit_cveRegistroCivil = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.memoEdit_DescripcionRegistroCivil = new DevExpress.XtraEditors.MemoEdit();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.simpleButton_aceptar = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.simpleButton_cancelar = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.checkEdit_estado = new DevExpress.XtraEditors.CheckEdit();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_cveRegistroCivil.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit_DescripcionRegistroCivil.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit_estado.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.checkEdit_estado);
+            this.layoutControl1.Controls.Add(this.simpleButton_cancelar);
+            this.layoutControl1.Controls.Add(this.simpleButton_aceptar);
+            this.layoutControl1.Controls.Add(this.memoEdit_DescripcionRegistroCivil);
+            this.layoutControl1.Controls.Add(this.textEdit_cveRegistroCivil);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(545, 188);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.emptySpaceItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4,
+            this.layoutControlItem5});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(545, 188);
+            this.Root.TextVisible = false;
+            // 
+            // textEdit_cveRegistroCivil
+            // 
+            this.textEdit_cveRegistroCivil.Location = new System.Drawing.Point(151, 12);
+            this.textEdit_cveRegistroCivil.Name = "textEdit_cveRegistroCivil";
+            this.textEdit_cveRegistroCivil.Size = new System.Drawing.Size(296, 22);
+            this.textEdit_cveRegistroCivil.StyleController = this.layoutControl1;
+            this.textEdit_cveRegistroCivil.TabIndex = 4;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.textEdit_cveRegistroCivil;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(439, 28);
+            this.layoutControlItem1.Text = "Clave de registro civil:";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(127, 16);
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 151);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(525, 17);
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // memoEdit_DescripcionRegistroCivil
+            // 
+            this.memoEdit_DescripcionRegistroCivil.Location = new System.Drawing.Point(151, 40);
+            this.memoEdit_DescripcionRegistroCivil.Name = "memoEdit_DescripcionRegistroCivil";
+            this.memoEdit_DescripcionRegistroCivil.Size = new System.Drawing.Size(382, 71);
+            this.memoEdit_DescripcionRegistroCivil.StyleController = this.layoutControl1;
+            this.memoEdit_DescripcionRegistroCivil.TabIndex = 5;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.memoEdit_DescripcionRegistroCivil;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 28);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(525, 75);
+            this.layoutControlItem2.Text = "Descripcion:";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(127, 16);
+            // 
+            // simpleButton_aceptar
+            // 
+            this.simpleButton_aceptar.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton1.ImageOptions.SvgImage")));
+            this.simpleButton_aceptar.Location = new System.Drawing.Point(12, 115);
+            this.simpleButton_aceptar.Name = "simpleButton_aceptar";
+            this.simpleButton_aceptar.Size = new System.Drawing.Size(258, 44);
+            this.simpleButton_aceptar.StyleController = this.layoutControl1;
+            this.simpleButton_aceptar.TabIndex = 6;
+            this.simpleButton_aceptar.Text = "Aceptar";
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.simpleButton_aceptar;
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 103);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(262, 48);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // simpleButton_cancelar
+            // 
+            this.simpleButton_cancelar.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton2.ImageOptions.SvgImage")));
+            this.simpleButton_cancelar.Location = new System.Drawing.Point(274, 115);
+            this.simpleButton_cancelar.Name = "simpleButton_cancelar";
+            this.simpleButton_cancelar.Size = new System.Drawing.Size(259, 44);
+            this.simpleButton_cancelar.StyleController = this.layoutControl1;
+            this.simpleButton_cancelar.TabIndex = 7;
+            this.simpleButton_cancelar.Text = "Cancelar";
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.simpleButton_cancelar;
+            this.layoutControlItem4.Location = new System.Drawing.Point(262, 103);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(263, 48);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // checkEdit_estado
+            // 
+            this.checkEdit_estado.Location = new System.Drawing.Point(451, 12);
+            this.checkEdit_estado.Name = "checkEdit_estado";
+            this.checkEdit_estado.Properties.Caption = "Activo";
+            this.checkEdit_estado.Size = new System.Drawing.Size(82, 24);
+            this.checkEdit_estado.StyleController = this.layoutControl1;
+            this.checkEdit_estado.TabIndex = 8;
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.checkEdit_estado;
+            this.layoutControlItem5.Location = new System.Drawing.Point(439, 0);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(86, 28);
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem5.TextVisible = false;
+            // 
+            // DatosEstadoCivil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 188);
+            this.Controls.Add(this.layoutControl1);
+            this.IconOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("DatosEstadoCivil.IconOptions.SvgImage")));
+            this.MaximizeBox = false;
+            this.Name = "DatosEstadoCivil";
+            this.Text = "DatosEstadoCivil";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit_cveRegistroCivil.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit_DescripcionRegistroCivil.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit_estado.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraEditors.TextEdit textEdit_cveRegistroCivil;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton_cancelar;
+        private DevExpress.XtraEditors.SimpleButton simpleButton_aceptar;
+        private DevExpress.XtraEditors.MemoEdit memoEdit_DescripcionRegistroCivil;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraEditors.CheckEdit checkEdit_estado;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+    }
+}
