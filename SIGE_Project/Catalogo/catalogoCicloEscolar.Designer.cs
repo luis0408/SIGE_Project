d8a8b230d65d50464a1860e4dfde69e845848688
AdalbertoBravo <adal809010@gmail.com>
2024-08-02 19:27:18 -0600

    Agregue catalogos -bachilleratos -codigo postal -colonia -discapacidad

--- SIGE_Project/Catalogo/catalogoCicloEscolar.Designer.cs
+++ SIGE_Project/Catalogo/catalogoCicloEscolar.Designer.cs
@@ -50,6 +50,8 @@
             this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.gaugeControl1 = new DevExpress.XtraGauges.Win.GaugeControl();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
             gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
@@ -60,26 +62,29 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
             this.SuspendLayout();
             // 
             // gridColumn1
             // 
             gridColumn1.Caption = "ID Ciclo Escolar";
             gridColumn1.FieldName = "idCicloEscolar";
+            gridColumn1.MinWidth = 30;
             gridColumn1.Name = "gridColumn1";
+            gridColumn1.Width = 112;
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Controls.Add(this.gaugeControl1);
             this.layoutControl1.Controls.Add(this.labelControl1);
             this.layoutControl1.Controls.Add(this.gridControl_cicloEscolar);
             this.layoutControl1.Controls.Add(this.navBarControl1);
             this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.layoutControl1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControl1.Margin = new System.Windows.Forms.Padding(2);
             this.layoutControl1.Name = "layoutControl1";
             this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(716, 233, 650, 400);
             this.layoutControl1.Root = this.Root;
-            this.layoutControl1.Size = new System.Drawing.Size(861, 584);
+            this.layoutControl1.Size = new System.Drawing.Size(1292, 854);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
@@ -87,21 +92,21 @@
             // 
             this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelControl1.Appearance.Options.UseFont = true;
-            this.labelControl1.Location = new System.Drawing.Point(12, 12);
+            this.labelControl1.Location = new System.Drawing.Point(18, 18);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(102, 19);
+            this.labelControl1.Size = new System.Drawing.Size(150, 29);
             this.labelControl1.StyleController = this.layoutControl1;
             this.labelControl1.TabIndex = 6;
             this.labelControl1.Text = "Ciclo escolar";
             // 
             // gridControl_cicloEscolar
             // 
-            this.gridControl_cicloEscolar.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(1);
-            this.gridControl_cicloEscolar.Location = new System.Drawing.Point(12, 35);
+            this.gridControl_cicloEscolar.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.gridControl_cicloEscolar.Location = new System.Drawing.Point(18, 53);
             this.gridControl_cicloEscolar.MainView = this.gridView_cicloEscolar;
-            this.gridControl_cicloEscolar.Margin = new System.Windows.Forms.Padding(2);
             this.gridControl_cicloEscolar.Name = "gridControl_cicloEscolar";
-            this.gridControl_cicloEscolar.Size = new System.Drawing.Size(663, 537);
+            this.gridControl_cicloEscolar.Size = new System.Drawing.Size(995, 757);
             this.gridControl_cicloEscolar.TabIndex = 5;
             this.gridControl_cicloEscolar.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView_cicloEscolar});
@@ -114,7 +119,7 @@
             this.gridColumn3,
             this.gridColumn4,
             this.gridColumn5});
-            this.gridView_cicloEscolar.DetailHeight = 239;
+            this.gridView_cicloEscolar.DetailHeight = 349;
             this.gridView_cicloEscolar.GridControl = this.gridControl_cicloEscolar;
             this.gridView_cicloEscolar.Name = "gridView_cicloEscolar";
             this.gridView_cicloEscolar.OptionsBehavior.Editable = false;
@@ -126,31 +131,39 @@
             // 
             this.gridColumn2.Caption = "Año Inicio";
             this.gridColumn2.FieldName = "anioInicio";
+            this.gridColumn2.MinWidth = 30;
             this.gridColumn2.Name = "gridColumn2";
             this.gridColumn2.Visible = true;
             this.gridColumn2.VisibleIndex = 0;
+            this.gridColumn2.Width = 112;
             // 
             // gridColumn3
             // 
             this.gridColumn3.Caption = "Año Fin";
             this.gridColumn3.FieldName = "anioFin";
+            this.gridColumn3.MinWidth = 30;
             this.gridColumn3.Name = "gridColumn3";
             this.gridColumn3.Visible = true;
             this.gridColumn3.VisibleIndex = 1;
+            this.gridColumn3.Width = 112;
             // 
             // gridColumn4
             // 
             this.gridColumn4.Caption = "Estado Num";
             this.gridColumn4.FieldName = "estado";
+            this.gridColumn4.MinWidth = 30;
             this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Width = 112;
             // 
             // gridColumn5
             // 
             this.gridColumn5.Caption = "Estado";
             this.gridColumn5.FieldName = "estadoLetra";
+            this.gridColumn5.MinWidth = 30;
             this.gridColumn5.Name = "gridColumn5";
             this.gridColumn5.Visible = true;
             this.gridColumn5.VisibleIndex = 2;
+            this.gridColumn5.Width = 112;
             // 
             // navBarControl1
             // 
@@ -164,11 +177,10 @@
             this.navBarItem_edit,
             this.navBarItem_status,
             this.navBarItem_export});
-            this.navBarControl1.Location = new System.Drawing.Point(679, 35);
-            this.navBarControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.navBarControl1.Location = new System.Drawing.Point(1019, 53);
             this.navBarControl1.Name = "navBarControl1";
-            this.navBarControl1.OptionsNavPane.ExpandedWidth = 170;
-            this.navBarControl1.Size = new System.Drawing.Size(170, 537);
+            this.navBarControl1.OptionsNavPane.ExpandedWidth = 255;
+            this.navBarControl1.Size = new System.Drawing.Size(255, 757);
             this.navBarControl1.TabIndex = 4;
             this.navBarControl1.Text = "navBarControl1";
             // 
@@ -233,26 +245,27 @@
             this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlItem1,
             this.layoutControlItem2,
-            this.layoutControlItem3});
+            this.layoutControlItem3,
+            this.layoutControlItem4});
             this.Root.Name = "Root";
-            this.Root.Size = new System.Drawing.Size(861, 584);
+            this.Root.Size = new System.Drawing.Size(1292, 854);
             this.Root.TextVisible = false;
             // 
             // layoutControlItem1
             // 
             this.layoutControlItem1.Control = this.navBarControl1;
-            this.layoutControlItem1.Location = new System.Drawing.Point(667, 23);
+            this.layoutControlItem1.Location = new System.Drawing.Point(1001, 35);
             this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(174, 541);
+            this.layoutControlItem1.Size = new System.Drawing.Size(261, 763);
             this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem1.TextVisible = false;
             // 
             // layoutControlItem2
             // 
             this.layoutControlItem2.Control = this.gridControl_cicloEscolar;
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 23);
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 35);
             this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(667, 541);
+            this.layoutControlItem2.Size = new System.Drawing.Size(1001, 763);
             this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem2.TextVisible = false;
             // 
@@ -261,18 +274,32 @@
             this.layoutControlItem3.Control = this.labelControl1;
             this.layoutControlItem3.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.Size = new System.Drawing.Size(841, 23);
+            this.layoutControlItem3.Size = new System.Drawing.Size(1262, 35);
             this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem3.TextVisible = false;
             // 
+            // gaugeControl1
+            // 
+            this.gaugeControl1.Location = new System.Drawing.Point(173, 816);
+            this.gaugeControl1.Name = "gaugeControl1";
+            this.gaugeControl1.Size = new System.Drawing.Size(1101, 20);
+            this.gaugeControl1.TabIndex = 7;
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.gaugeControl1;
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 798);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(1262, 26);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(137, 19);
+            // 
             // catalogoCicloEscolar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(861, 584);
+            this.ClientSize = new System.Drawing.Size(1292, 854);
             this.Controls.Add(this.layoutControl1);
             this.IconOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("catalogoCicloEscolar.IconOptions.SvgImage")));
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "catalogoCicloEscolar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ciclo escolar";
@@ -286,6 +313,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -312,5 +340,7 @@
         private DevExpress.XtraNavBar.NavBarItem navBarItem_export;
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraGauges.Win.GaugeControl gaugeControl1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
     }
 }
